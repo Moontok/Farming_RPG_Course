abe8c435d22dd29e2810a3ecb82bb00847aa3bd0
Moontok <zspink@nea.k12.ar.us>
2020-09-23 09:25:21 -0500

    First

---
+++ RPG_Farming_Course/Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,33 @@
+﻿// public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+// ToolEffect toolEffect, 
+// bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+// bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+// bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+// bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+// bool idleUp, bool idleDown, bool idleRight, bool idleLeft
+// );
+
+// public static class EventHandler
+// {
+//     // Movement Event
+
+//     public static event MovementDelegate MovementEvent;
+
+//     // Movement Event Call for Publishers
+//     public static void CallMovementEvent()
+//     {
+//         if(MovementEvent != null)
+//         {
+
+//         }
+//     }
+// }
+
+public enum ToolEffect { none, Watering }
+public enum PlayerState { Idle, Walking, Running }
+public enum PlayerAction { Idle, UsingTool, LiftingTool, Picking, SwingingTool }
+public enum PlayerActionDirection { Left, Right, Up, Down }
+
+public delegate void MovementDelegate(float inputX, float inputY, 
+PlayerState playerState, PlayerAction playerAction, PlayerActionDirection playerActionDirection,
+bool isCarrying, ToolEffect toolEffect);
---
+++ RPG_Farming_Course/Assets/Scripts/Miscs/SingletonMonobehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonobehavior<T> : MonoBehaviour where T:MonoBehaviour
+{
+    private static T instance = null;
+
+    public static T Instance
+    { 
+        get 
+        {
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
